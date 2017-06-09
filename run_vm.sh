2dc155daa65172a170fd53eb77af3e66d9f4b457
Manjeet Singh Bhatia <manjeet.s.bhatia@intel.com>
2017-06-09 19:45:27 +0000

    add carbon working conf

--- run_vm.sh
+++ run_vm.sh
@@ -4,7 +4,7 @@ neutron security-group-rule-create   \
         --protocol icmp              \
         --direction ingress          \
         --remote-ip-prefix 0.0.0.0/0 \
-        92942417-0fa9-4344-8c7e-8ef73a812646
+	3f682690-2704-4f3b-8e92-5f48e4f6da42
 
 neutron security-group-rule-create   \
         --protocol tcp               \
@@ -12,7 +12,7 @@ neutron security-group-rule-create   \
         --port-range-max 22          \
         --direction ingress          \
         --remote-ip-prefix 0.0.0.0/0 \
-        92942417-0fa9-4344-8c7e-8ef73a812646
+	3f682690-2704-4f3b-8e92-5f48e4f6da42
 
 nova boot --flavor m1.tiny --image cirros-0.3.5-x86_64-disk --nic net-id=`neutron net-list | grep private | awk '{print $2}'` --max-count 1 test
 
